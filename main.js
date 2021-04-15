94cb47536d5f2ff137cbbec88942c674c52877c2
Terezia Gondova <terezia.gondova@gmail.com>
2021-04-15 13:07:46 +0200

    [FEATURE] main functionality of "sportka" implemented

--- main.js
+++ main.js
@@ -6,3 +6,27 @@
 // Z každého vylosovaného čísla vygeneruj následující HTML kód:
 // <span class="cislo">8</span>
 // ... který pak přidej dovnitř prvku <div id="vyherni-cisla">:
+
+let osudi = [];
+
+for(let i = 1; i <= 48; i++){
+    osudi.push(i);
+}
+
+// z osudi vyberu 7 nahodnych cisel
+
+// vypisu do HTML
+let tazenaCisla = [];
+
+for (let i = 0; i < 7; i++){
+    let vyherniIndex = Math.floor(Math.random() * osudi.length);
+    let vyherniCislo = osudi[vyherniIndex]
+
+    tazenaCisla.push(vyherniCislo);
+    osudi.splice(vyherniIndex,1);
+
+    console.log(osudi);
+    console.log(tazenaCisla);
+}
+
+
